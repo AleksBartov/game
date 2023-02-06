051895ac9d69ea5c097d5da819cd3a8d3693ecb5
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2023-02-07 00:41:14 +0300

    start creating obstacles

--- script.js
+++ script.js
@@ -50,12 +50,33 @@ window.addEventListener('load', function() {
     }
   }
 
+  class Obstacle {
+    constructor( game ) {
+      this.game = game;
+      this.collisionX = Math.random() * this.game.width;
+      this.collisionY = Math.random() * this.game.height;
+      this.collisionRadius = 80;
+    }
+
+    draw(context) {
+      context.beginPath();
+      context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.restore();
+      context.stroke();
+    }
+  }
+
   class Game {
     constructor(canvas) {
         this.canvas = canvas;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+        this.numOfObstacles = 6;
+        this.obstacles = [];
 
         this.mouse = {
           x: this.width * 0.5,
@@ -83,10 +104,18 @@ window.addEventListener('load', function() {
     render(context) {
       this.player.draw(context);
       this.player.update();
+      this.obstacles.forEach(obstacle => obstacle.draw(context));
+    }
+
+    init() {
+      for (let i = 0; i < this.numOfObstacles; i++) {
+        this.obstacles.push(new Obstacle(this));
+      }
     }
   }
 
   const game = new Game(canvas);
+  game.init();
   
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
