792130ac330bdace488da805659d9558fcaa5c19
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2023-03-15 22:59:50 +0300

    setting backtiks

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ addEventListener('load', function(e) {
 
   const words = consonants.flatMap(con=> {
     return vowels.map(l=> {
-      return ${con}${l}
+      return `${con}${l}`
     })
   }).reduce((acc, l)=>{
      let random = Math.floor(Math.random()*acc.length);
@@ -23,11 +23,11 @@ addEventListener('load', function(e) {
     card.classList.add('card');
     const content = document.createElement('div');
     content.classList.add('content');
-    content.innerHTML = ${w};
+    content.innerHTML = `${w}`;
     
  // ------transforming----------
-    card.style.transform = translateZ(${i*.1}px) rotateX(30deg) rotateZ(${Math.floor(Math.random()*24-12)}deg);
-    card.style.zIndex = ${i*100};
+    card.style.transform = `translateZ(${i*.1}px) rotateX(30deg) rotateZ(${Math.floor(Math.random()*24-12)}deg)`;
+    card.style.zIndex = `${i*100}`;
 
     card.appendChild(content);
 
@@ -41,19 +41,19 @@ addEventListener('load', function(e) {
       e.preventDefault();
       offsetX = e.changedTouches[0].screenX;
       offsetY = e.changedTouches[0].screenY;
-      c.style.transform = rotateX(0deg) rotateZ(0deg) translateZ(150px);
+      c.style.transform = `rotateX(0deg) rotateZ(0deg) translateZ(150px)`;
       c.style.filter = 'blur(0px)';
     });
     c.addEventListener('touchmove', e => {
      e.preventDefault();
-     c.style.transform = rotateX(0deg) rotateZ(0deg) translateZ(150px) translateX(${e.changedTouches[0].screenX-offsetX}px) translateY(${e.changedTouches[0].screenY-offsetY}px);
+     c.style.transform = `rotateX(0deg) rotateZ(0deg) translateZ(150px) translateX(${e.changedTouches[0].screenX-offsetX}px) translateY(${e.changedTouches[0].screenY-offsetY}px)`;
  
     endX = e.changedTouches[0].screenX-offsetX;
 
     });
     c.addEventListener('touchend', e => {
      e.preventDefault();
-     c.style.transform = rotateX(0deg) rotateZ(0deg) translateZ(150px) translateX(${endX > 0 ? 500 : -500}px) translateY(${e.changedTouches[0].screenY-offsetY}px);
+     c.style.transform = `rotateX(0deg) rotateZ(0deg) translateZ(150px) translateX(${endX > 0 ? 500 : -500}px) translateY(${e.changedTouches[0].screenY-offsetY}px)`;
     endX = 0;
 
     });
