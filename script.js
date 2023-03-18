4e0b115805ba6754f33f2d38a61771d26527d73f
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2023-03-18 08:58:44 +0300

    Update script.js
    Disable test recognition

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ addEventListener('load', function(e) {
   recognition.maxAlternatives = 3;
   recognition.lang = "ru-RU";
 
+
+/*
   recognition.onstart = () => {
     console.log("Распознавание голоса запущено");
     // alert('listening...');
@@ -17,6 +19,8 @@ addEventListener('load', function(e) {
   };
 
   recognition.onstart();
+  
+  */
 
   let offsetX, offsetY, endX = 0, endListen = false;
  
@@ -72,10 +76,10 @@ addEventListener('load', function(e) {
     });
     c.addEventListener('touchend', e => {
      e.preventDefault();
-     if(!endListen) {
+    /* if(!endListen) {
       recognition.onend();
       endListen = true;
-     }
+     } */
      c.style.transform = `rotateX(0deg) rotateZ(0deg) translateZ(150px) translateX(${endX > 0 ? 500 : -500}px) translateY(${e.changedTouches[0].screenY-offsetY}px)`;
     endX = 0;
 
